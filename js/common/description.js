f1a76ee5acc45c1214296377d7877bac129fdf72
Lu Yihui <lu_yihui@126.com>
2019-10-25 15:34:52 +0800

    unify header&footer, using js

---
+++ js/common/description.js
@@ -0,0 +1,8 @@
+document.writeln("<meta charset=\'utf-8\'>");
+document.writeln("    <meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0\'>");
+document.writeln("    <meta name=\'description\' content=\'Developed By M Abdur Rokib Promy\'>");
+document.writeln("    <meta name=\'author\' content=\'cosmic\'>");
+document.writeln("    <meta name=\'keywords\' content=\'SRDS, distributed system, conference, ieee, operating system\'>");
+document.writeln("    <link rel=\'shortcut icon\' href=\'img/tag_icon.jpg\'>");
+document.writeln("");
+document.writeln("    <title>SRDS 2020 | Shanghai, China</title>");
