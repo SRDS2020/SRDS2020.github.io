b66a0b7eab53fe568c0564c8471bb9dd729c5fb0
Lu Yihui <lu_yihui@126.com>
2019-10-29 16:01:27 +0800

    inhibit server cache

--- js/common/description.js
+++ js/common/description.js
@@ -3,9 +3,9 @@ document.writeln("    <meta name=\'viewport\' content=\'width=device-width, init
 document.writeln("    <meta name=\'description\' content=\'Developed By M Abdur Rokib Promy\'>");
 document.writeln("    <meta name=\'author\' content=\'cosmic\'>");
 document.writeln("    <meta name=\'keywords\' content=\'SRDS, distributed system, conference, ieee, operating system\'>");
-document.writeln("<meta http-equiv=\'pragma\' content=\'no-cache\'> ");
-document.writeln("<meta http-equiv=\'Cache-Control\' content=\'no-cache, must-revalidate\'> ");
-document.writeln("<meta http-equiv=\'expires\' content=\'Wed, 26 Feb 1997 08:21:57 GMT\'>");
+document.writeln("    <meta http-equiv=\'pragma\' content=\'no-cache\'> ");
+document.writeln("    <meta http-equiv=\'Cache-Control\' content=\'no-cache, no-store, must-revalidate\'> ");
+document.writeln("    <meta http-equiv=\'expires\' content=\'Wed, 26 Feb 1997 08:21:57 GMT\'>");
 document.writeln("    <link rel=\'shortcut icon\' href=\'img/tag_icon.png\'>");
 document.writeln("");
 document.writeln("    <title>SRDS 2020 | Shanghai, China</title>");
